d4425e7c9a6df9bfb17e50f71b74cc5c3def68fc
Nicolas VAREILLE <nicolas.vareille@epitech.eu>
2015-10-25 22:19:35 +0900

    Add files

---
+++ Runescape Clicker/Views/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Runescape_Clicker.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NewPatternViewSummon(object sender, RoutedEventArgs e)
+        {
+            NewPatternView content = new NewPatternView();
+            
+            content.Show();
+        }
+
+        private void LoadPatternViewSummon(object sender, RoutedEventArgs e)
+        {
+
+        }
+        
+        private void DeletePatternViewSummon(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
